a53c3a12b35c33d0fd889f53bc1b3013cfeb2e7c
giiaga <ghousali99@gmail.com>
2020-10-28 19:06:10 -0400

    making progress

--- problems/graph.js
+++ problems/graph.js
@@ -33,25 +33,38 @@ class Graph {
       if (!this.adjList[srcValue]) {
         this.addVertex(srcValue);
       };
-
       this.adjList[srcValue] = [...this.adjList[srcValue], destValue];
       this.adjList[destValue] = [...this.adjList[destValue], srcValue];
-
-}
+};
 
 buildGraph(edges) {
 for (let i = 0 ; i < edges.length; i++) {
-  this.addEdges(edges[i][0], edges[i][1])
+  if (edges[i].length <= 1) {
+    this.addVertex(edges[i][0]);
+  } else {
+    this.addEdges(edges[i][0], edges[i][1])
+}
 }
 return this.adjList
 }
 
-breadthFirstTraversal(startingVertex) {
-  // Code goes here ...
+breadthFirstTraversal(startingVertex) { //"a" "h" adjlist[h]
+  let visited = new Set();
+  let queue = [startingVertex]; //[e, a,b,f,g,a,g,b]
+  while (queue.length) {
+    let current = queue.shift();
+    if(!visited.has(current)) {
+      visited.add(current);
+      queue.push(...this.adjList[current]);
+    }
+  }
+
+return Array.from(visited);
 }
 
+
 depthFirstTraversalIterative(startingVertex) {
-  // Code goes here ...
+
 }
 
 depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
